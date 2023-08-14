870771d3f2c863e7fc576f55024c2147ead5c3e1
along <1433799685@qq.com>
2023-08-14 19:09:51 +0800

    leetcode刷题初始化

---
+++ src/main/java/com/italong/leetcode/Main.java
@@ -0,0 +1,13 @@
+package com.italong.leetcode;
+
+/**
+ * Create by Idea
+ *
+ * @Author along
+ * @create 2023/8/14 19:08
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
