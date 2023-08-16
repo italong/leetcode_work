3f59e2a30737d860fe46fae0583825af7be2e275
along <1433799685@qq.com>
2023-08-16 18:50:39 +0800

    leetcode_day03无重复字符的最长字串

---
+++ src/main/java/com/italong/leetcode/day03/Solution.java
@@ -0,0 +1,24 @@
+package com.italong.leetcode.day03;
+
+/**
+ * Create by Idea
+ *  给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ * @Author along
+ * @create 2023/8/16 16:59
+ */
+public class Solution {
+    public int lengthString(String s){
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            boolean[] book = new boolean[300];
+            for (int j = i; j >= 0; j--) {
+              if (book[s.charAt(j)]) {
+                  break;
+              }
+              book[s.charAt(j)] = true;
+              count = Math.max(count,i-j+1);
+            }
+        }
+        return count;
+    }
+}
