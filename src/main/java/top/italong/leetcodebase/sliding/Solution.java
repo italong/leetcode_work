e5b62c4085d9016259a7a37f19705ceb6c53c856
along <1433799685@qq.com>
2023-08-25 21:37:19 +0800

    leetcode_base滑动窗口

---
+++ src/main/java/top/italong/leetcodebase/sliding/Solution.java
@@ -0,0 +1,21 @@
+package top.italong.leetcodebase.sliding;
+
+/**
+ * Create by Idea
+ * 2.滑动窗口的最大值
+ * @Author along
+ * @create 2023/8/25 21:34
+ */
+public class Solution {
+    /**
+     * 二分查找
+     * @param n int整型 数组长度
+     * @param v int整型 查找值
+     * @param a int整型一维数组 有序数组
+     * @return int整型
+     */
+    public int upper_bound_ (int n, int v, int[] a) {
+        // write code here
+        return 0;
+    }
+}
