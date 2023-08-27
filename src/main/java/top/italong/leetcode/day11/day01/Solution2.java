c8fe66d901688c90e5d37dfceac7738fbccbdca4
along <1433799685@qq.com>
2023-08-27 21:38:53 +0800

    leetcode_day11盛最多的水

---
+++ src/main/java/top/italong/leetcode/day11/day01/Solution2.java
@@ -0,0 +1,22 @@
+package top.italong.leetcode.day11.day01;
+
+/**
+ * Create by Idea
+ *
+ * @Author along
+ * @create 2023/8/27 21:38
+ */
+public class Solution2 {
+    public int maxArea(int[] height){
+        int lef = 0,rig = height.length - 1;
+        int res = 0;
+        while(lef <= rig){
+            res = Math.max(res,(rig - lef) * Math.min(height[lef],height[rig]));
+            if(height[lef] <= height[rig])
+                lef++;
+            else
+                rig--;
+        }
+        return res;
+    }
+}
