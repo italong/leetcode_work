08817bd36549d258e9b037bf68a97be80204a379
along <1433799685@qq.com>
2023-09-01 15:12:23 +0800

    leetcode_base滑动窗口的最大值

--- src/main/java/top/italong/leetcodebase/sliding/Solution.java
+++ src/main/java/top/italong/leetcodebase/sliding/Solution.java
@@ -1,5 +1,7 @@
 package top.italong.leetcodebase.sliding;
 
+import java.util.LinkedList;
+
 /**
  * Create by Idea
  * 2.滑动窗口的最大值
@@ -7,15 +9,32 @@ package top.italong.leetcodebase.sliding;
  * @create 2023/8/25 21:34
  */
 public class Solution {
-    /**
-     * 二分查找
-     * @param n int整型 数组长度
-     * @param v int整型 查找值
-     * @param a int整型一维数组 有序数组
-     * @return int整型
-     */
-    public int upper_bound_ (int n, int v, int[] a) {
-        // write code here
-        return 0;
-    }
+
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            if (nums.length == 0 || k == 0) {
+                    return new int[0];
+            }
+            int n = nums.length;
+            LinkedList<Integer> deque = new LinkedList<>();
+            for (int i = 0; i < k; i++) {
+               while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]){
+                    deque.pollLast();
+               }
+               deque.offerLast(i);
+            }
+            int[] ans = new int[n - k + 1];
+            ans[0] = nums[deque.peekFirst()];
+            for (int i = 0; i < n; i++) {
+                while (!deque.isEmpty() && nums[i] >=nums[deque.peekLast()]) {
+                        deque.peekLast();
+                }
+                deque.offerLast(i);
+                while (deque.peekFirst() <= i - k) {
+                        deque.pollFirst();
+                }
+                ans[i - k + 1] = nums[deque.peekFirst()];
+            }
+            return ans;
+        }
+
 }
