83216999a55686b47e665d0566eb2a3fb6a20aeb
along <1433799685@qq.com>
2023-09-04 10:18:31 +0800

    leetcode_day18四数之和

--- src/main/java/top/italong/leetcode/day20/day08/Solution1.java
+++ src/main/java/top/italong/leetcode/day20/day08/Solution1.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 /**
  * Create by Idea
- * 18.四数之和
+ *  18.四数之和
  * @Author along
- * @create 2023/9/4 9:43
+ * @create 2023/9/4 9:56
  */
-public class Solution {
+public class Solution1 {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         ArrayList<List<Integer>> res = new ArrayList<>();
         if (nums.length <  4){
@@ -20,6 +20,12 @@ public class Solution {
         for (int i = 0; i < nums.length - 3; i++) {
             if (i > 0 && nums[i] == nums[i-1]){
                 continue;
+            }
+            if ((long)nums[i]+nums[nums.length-3]+nums[nums.length-2]+nums[nums.length-1] < (long) target){
+                continue;
+            }
+            if ((long)nums[i]+nums[i+1]+nums[i+2]+nums[i+3] > (long) target){
+                break;
             }
             for (int j = i+1; j < nums.length - 2; j++) {
                 if (j > i+1 && nums[j] == nums[j-1]){
