8a918fd85edc9661858fda563d9cfdd6f2412ace
along <1433799685@qq.com>
2023-09-05 15:14:46 +0800

    leetcode_day02两数相加

--- src/main/java/top/italong/leetcode/day10/day02/ListNode.java
+++ src/main/java/top/italong/leetcode/day10/day02/ListNode.java
@@ -14,7 +14,7 @@ package top.italong.leetcode.day10.day02;
  */
 public class ListNode {
     int val;
-    ListNode next;
+    public ListNode next;
     public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
