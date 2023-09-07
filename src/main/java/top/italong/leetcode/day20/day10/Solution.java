432f9b696e1fa09f410c6fb11799dd01ab358196
along <1433799685@qq.com>
2023-09-07 08:54:03 +0800

    leetcode_day20有效的括号

--- src/main/java/top/italong/leetcode/day20/day10/Solution.java
+++ src/main/java/top/italong/leetcode/day20/day10/Solution.java
@@ -11,22 +11,22 @@ import java.util.Stack;
 public class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        for(int i = 0;i < s.length();i++){
-            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
                 st.push(s.charAt(i));
-            else{
-                if(st.size() == 0)
+            else
+                if (st.size() == 0)
                     return false;
                 char com = st.pop();
-                if(com == '(' && s.charAt(i) == ')'
-                        || com == '[' && s.charAt(i) == ']'
-                        || com == '{' && s.charAt(i) == '}')
+                if (com == '(' && s.charAt(i) == ')'
+                || com == '[' && s.charAt(i) == ']'
+                || com == '{' && s.charAt(i) == '}')
                     continue;
                 else
                     return false;
-            }
         }
         return st.size() == 0;
     }
+
 
 }
