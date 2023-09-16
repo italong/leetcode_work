a0dbcd4145f4e8170b6a8f71165ff3eb5aeacb24
italong <1433799685@qq.com>
2023-09-16 23:52:19 +0800

    leetcode_day28找出字符串匹配的第一个下标

--- src/main/java/top/italong/leetcode/day30/day08/Solution1.java
+++ src/main/java/top/italong/leetcode/day30/day08/Solution1.java
@@ -2,9 +2,30 @@ package top.italong.leetcode.day30.day08;
 
 /**
  * Create by Idea
- *
+ * 28.实现strStr()
  * @Author along
  * @create 2023/9/16 11:13
  */
 public class Solution1 {
+    public static void main(String[] args) {
+        String test = "mississippi";
+        String test2 = "issipi";
+        System.out.println(test.substring(0,test2.length()));
+        System.out.println(subStr(test,test2));
+    }
+    public static int subStr(String haystack,String needle){
+        if (haystack.length() < needle.length()) return 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)){
+                String count = haystack.substring(i,i+needle.length());
+                if (count.equals(needle)) {
+                    return i;
+                }
+                if (i+needle.length() > haystack.length()){
+                    return -1;
+                }
+            }
+        }
+        return -1;
+    }
 }
