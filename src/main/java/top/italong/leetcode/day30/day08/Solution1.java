dd25e3c07fd0250e61b9a96080c09e0911f909c5
italong <1433799685@qq.com>
2023-09-17 20:59:52 +0800

    leetcode_day28实现strStr()

--- src/main/java/top/italong/leetcode/day30/day08/Solution1.java
+++ src/main/java/top/italong/leetcode/day30/day08/Solution1.java
@@ -9,21 +9,22 @@ package top.italong.leetcode.day30.day08;
 public class Solution1 {
     public static void main(String[] args) {
         String test = "mississippi";
-        String test2 = "issipi";
+        String test2 = "issi";
         System.out.println(test.substring(0,test2.length()));
         System.out.println(subStr(test,test2));
     }
     public static int subStr(String haystack,String needle){
-        if (haystack.length() < needle.length()) return 0;
+        if (haystack.length() < needle.length()) return -1;
         for (int i = 0; i < haystack.length(); i++) {
+            if (i+needle.length() > haystack.length()){
+                return -1;
+            }
             if (haystack.charAt(i) == needle.charAt(0)){
                 String count = haystack.substring(i,i+needle.length());
                 if (count.equals(needle)) {
                     return i;
                 }
-                if (i+needle.length() > haystack.length()){
-                    return -1;
-                }
+
             }
         }
         return -1;
